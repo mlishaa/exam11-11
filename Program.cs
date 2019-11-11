56c12407bac030727b26026d79c502ba1b29ef54
mac <mac@DESKTOP-T73Q40O>
2019-11-11 15:35:50 -0500

    ffff

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutorials
+{
+    //struct xPoint
+    //{
+    //   public int x;
+    //   public int y;
+    //}
+    class Program
+    {
+        //enum colors
+        //{
+        //    RED = 1, // 1
+        //    BLUE = 2, //2
+        //    ORANGE = 3//3
+        //}
+
+       public static void Main(string[] args)
+        {
+            //foreach (String icolor in Enum.GetNames(typeof(colors)))
+            //{
+            //    Console.Write(icolor + " ");
+            //}
+            
+            // xPoint[] arr=new xPoint[5];
+            //arr[0].x = 5;
+            //arr[0].y = 1;
+
+            String[] mySTr = { "mike", "Vero", "Nadia" };
+
+            LinkedList<String> myLinkList = new LinkedList<string>(mySTr);
+            myLinkList.AddLast("Christina");
+
+            foreach(String name in myLinkList)
+            {
+                Console.Write(name + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(myLinkList.First.Value);
+            Console.WriteLine(myLinkList.Contains("Christina"));
+
+            
+
+            SortedDictionary<String, int> sortedList = new SortedDictionary<string, int>();
+            String[] names = { "mike", "mike", "malak", "malak", "mom", "mom", "mary" };
+
+            for(int i = 0; i < names.Length; ++i)
+            {
+                if (sortedList.ContainsKey(names[i]))
+                {
+                    int count = sortedList[names[i]];
+                    sortedList.Remove(names[i]);
+                    count++;
+                    sortedList.Add(names[i], count);
+                }
+                else
+                    sortedList.Add(names[i], 1);
+            }
+
+            foreach(KeyValuePair<String,int> pair in sortedList)
+            {
+                Console.WriteLine("Name {0} coours {1} times ",pair.Key,pair.Value);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("tutorials")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tutorials")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("85d2f59a-a4f8-427d-96bc-6d2c781afe66")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
